dad88084949830508ecd051ec54b61edd0013b5a
Bhumika Rana <164107781+Bhumikarana75@users.noreply.github.com>
2024-04-25 16:29:15 +0530

    Add files via upload

---
+++ String/p1.c
@@ -0,0 +1,10 @@
+//WAP to convert the given string in UPPERCASE using any string function.
+
+#include<stdio.h>
+
+main(){
+	
+	char str[]="Hello world";
+	
+	printf("%s ",strupr(str));
+}
---
+++ String/p2.c
@@ -0,0 +1,10 @@
+//WAP to convert the given string in lowercase using any string function.
+
+#include<stdio.h>
+
+main(){
+	
+	char str[] = "HELLO WORLD";
+	
+	printf("%s ",strlwr(str));
+}
